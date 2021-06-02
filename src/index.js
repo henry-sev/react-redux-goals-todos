50f17434e8f8c79f1a8764f546c0999719544875
henry-sev <q2466351926@gmail.com>
2021-06-02 18:02:59 +0800

    Render with store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import reducer from './reducers'
+import middleware from './middleware'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+
+
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider value={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
